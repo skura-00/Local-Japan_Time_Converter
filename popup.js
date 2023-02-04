0f5bd686bd8b9e433a371092f58ad26821da520d
Sachi K <105990444+skura-00@users.noreply.github.com>
2023-02-03 23:27:29 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,29 @@
 
+window.onload = function() {
+  const today = new Date();
+  document.getElementById("year").value = today.getFullYear();
+  document.getElementById("year2").value = today.getFullYear();
+
+  document.getElementById("month").value = today.getMonth()+1;
+  document.getElementById("month2").value = today.getMonth()+1;
+
+  document.getElementById("day").value = today.getDate();
+  document.getElementById("day2").value = today.getDate();
+
+  document.getElementById("hour").value = today.getHours();
+  document.getElementById("hour2").value = today.getHours();
+
+  document.getElementById("minute").value = today.getMinutes();
+  document.getElementById("minute2").value = today.getMinutes();
+}
+
 function errorValue(yr,mon,day,hr,min) {
   let isError = false;
-  if(yr < 2000 || yr > 2060) {
+  if(yr <= 0 || yr >= 2100) {
     isError = true;
     alert("Year is invalid.");
   }
-  if(mon < 1|| mon > 12) {
+  if(mon < 1 || mon > 12) {
     isError = true;
     alert("Month is invalid.");
   }
@@ -30,14 +48,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
   clicked.addEventListener('click', function() {
     const yr = document.getElementById("year").value;
-    const mon = document.getElementById("month").value-1;
+    const mon = document.getElementById("month").value;
     const day = document.getElementById("day").value;
     const hr = document.getElementById("hour").value;
     const min = document.getElementById("minute").value;
 
     let isError = errorValue(yr,mon,day,hr,min);
 
-    const d = new Date(yr,mon,day,hr,min);
+    const d = new Date(yr,mon-1,day,hr,min);
     if(isError) 
       document.getElementById("box").innerHTML = "Error: Invalid input";
     else 
